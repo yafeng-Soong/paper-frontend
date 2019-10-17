84a42b3965945db2e60c6ad1c1c866b14591ea89
yafeng-Soong <397655952@qq.com>
2019-10-17 16:59:59 +0800

    测试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="欢迎来到vue项目首页哦"/>
   </div>
 </template>
 
