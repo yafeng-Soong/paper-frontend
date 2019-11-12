165682a6e452fb16135fb358762ee35745d82930
yafeng-Soong <397655952@qq.com>
2019-11-12 09:00:57 +0800

    2019.11.13 songyafeng 修改了下载文件的方式，以前上传的文件可能下载不了，新上传的可以下载。被撤回的文件不能下载

--- src/views/paper/paperInfo/Download.vue
+++ src/views/paper/paperInfo/Download.vue
@@ -2,16 +2,26 @@
   <div>
     <el-row :gutter="12">
       <el-col :span="3"><div class="el-col bg-purple">原稿下载</div></el-col>
-      <v-btn text icon color="success" title="下载" @click="downloadFile()" style="margin-top: 4px">
+      <v-btn
+        :disabled="!downloadAble"
+        text
+        icon
+        color="success"
+        title="下载"
+        @click="downloadFile()"
+        style="margin-top: 4px">
         <v-icon>cloud_download</v-icon>
       </v-btn>
+      <v-chip v-if="!downloadAble" outlined>已撤回不能下载</v-chip>
     </el-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import paperApi from "@/api/paperApi.js"
+// import paperApi from "@/api/paperApi.js"
+import axios from 'axios';
+
 
 export default {
   name: 'download',
@@ -25,24 +35,33 @@ export default {
   mounted: function () {
     this.paperInfo = this.$store.getters.getPaperInfo;
   },
+  computed: {
+    downloadAble(){
+      return this.paperInfo.checkStatus !== '已撤回';
+    }
+  },
   methods:{
     downloadFile(){
-      paperApi.paperDownload(this.paperInfo.id)
-        .then(res => {
-          let filename = this.paperInfo.name;
-          const url = window.URL.createObjectURL(new Blob([res]));
-          const link = document.createElement('a');
-          link.href = url;
-          link.setAttribute('download', filename);
-          document.body.appendChild(link);
-          link.click();
-          document.body.removeChild(link);
-          window.URL.revokeObjectURL(url);
-          })
-        .catch(err => {
-          console.log(err)
-          this.$toast.error("下载文件出错！")
-        })
+      let url = this.$store.getters.getBaseUrl + this.paperInfo.filePath;
+      let that = this
+      axios({
+        url: url,
+        method: 'GET',
+        responseType: 'blob',
+      }).then(res => {
+        let filename = that.paperInfo.filePath.split("/").pop();
+        const url = window.URL.createObjectURL(new Blob([res.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', filename);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(url);
+      }).catch(err => {
+        console.log(err)
+        this.$toast.error("下载文件出错！")
+      });
     }
   }
 }
